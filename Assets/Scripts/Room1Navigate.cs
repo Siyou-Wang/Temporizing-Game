fe1f8cb7fd07ec7cbb619eeea513a83fbec6ea4a
Siyou-Wang <SiyouWang1@gmail.com>
2025-05-29 22:22:15 -0700

    Navigation done! Work on dialouge next. Still need to finish boss fight and figure out player movement

--- Assets/Scripts/Room1Navigate.cs
+++ Assets/Scripts/Room1Navigate.cs
@@ -7,8 +7,12 @@ public class Room1Navigate : MonoBehaviour
 {
     private DialougeManager dMan;
     private string[] message = new string[1];
-        private bool textShow = false;
+    private bool textShow = false;
     private int day = 0;
+
+    private bool d1Done = false;
+    private bool d2Done = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,37 +32,70 @@ public class Room1Navigate : MonoBehaviour
                 if (hit.collider.gameObject.tag == "House") {
                     Camera.main.transform.position = new Vector3(0, 20, -10);
                 }
-                if (hit.collider.gameObject.tag == "Attic")
+                if (hit.collider.gameObject.tag == "Attic" && day >= 3)
                 {
+                    Camera.main.transform.position = new Vector3(-40, 20, -10);
+                }
+                else if (hit.collider.gameObject.tag == "Attic") {
 
                 }
                 if (hit.collider.gameObject.tag == "Bed")
                 {
-
+                    day++;
+                    
                 }
                 if (hit.collider.gameObject.tag == "Drawer1")
                 {
-
+                    d1Done = true;
+                    if (d1Done && d2Done) {
+                        Camera.main.transform.position = new Vector3(40, 40, -10);
+                    }
                 }
                 if (hit.collider.gameObject.tag == "Drawer2")
                 {
-
+                    d2Done = true;
+                    if (d1Done && d2Done) {
+                        Camera.main.transform.position = new Vector3(40, 40, -10);
+                    }
                 }
                 if (hit.collider.gameObject.tag == "Door1")
                 {
                     Camera.main.transform.position = new Vector3(20, 20, -10);
                 }
-                if (hit.collider.gameObject.tag == "Door2")
+                if (hit.collider.gameObject.tag == "Door2" && day >= 1)
                 {
-
+                    Camera.main.transform.position = new Vector3(0, 40, -10);
+                }else if (hit.collider.gameObject.tag == "Door2")
+                {
+                   
                 }
-                if (hit.collider.gameObject.tag == "Door3")
+                if (hit.collider.gameObject.tag == "Door3" && day >= 2)
                 {
+                    Camera.main.transform.position = new Vector3(40, 20, -10);
+                }else if (hit.collider.gameObject.tag == "Door3") {
 
                 }
                 if (hit.collider.gameObject.tag == "Stairs")
                 {
-
+                    Camera.main.transform.position = new Vector3(-20, 20, -10);
+                }
+                if (hit.collider.gameObject.tag == "BackLeft") {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x-20, Camera.main.transform.position.y, -10);
+                }
+                if (hit.collider.gameObject.tag == "BackRight")
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x + 20, Camera.main.transform.position.y, -10);
+                }
+                if (hit.collider.gameObject.tag == "BackFarLeft")
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x - 40, Camera.main.transform.position.y, -10);
+                }
+                if (hit.collider.gameObject.tag == "BackDown")
+                {
+                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y -20, -10);
+                }
+                if (hit.collider.gameObject.tag == "BulltinBoard") {
+                    Camera.main.transform.position = new Vector3(0, 60, -10);
                 }
             }
         }
---
+++ Assets/riddlePaperGame.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riddlePaperGame : MonoBehaviour
+{
+    private Transform upperLeft;
+    private Transform upperRight;
+    private Transform lowerLeft;
+    private Transform lowerRight;
+
+    private Transform upperLeftPos;
+    private Transform upperRightPos;
+    private Transform lowerLeftPos;
+    private Transform lowerRightPos;
+
+    private string[] puzzleComplete = { "TEXT"};
+    private bool text = false;
+
+    private bool paperUpperLeft = false;
+    private bool paperUpperRight = false;
+    private bool paperLowerLeft = false;
+    private bool paperLowerRight = false;
+
+    private DialougeManager dMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upperLeft = GameObject.Find("riddlePaperUpperLeft").transform;
+        upperRight = GameObject.Find("riddlePaperUpperRight").transform;
+        lowerLeft = GameObject.Find("riddlePaperBottomLeft").transform;
+        lowerRight = GameObject.Find("riddlePaperBottomRight").transform;
+
+        upperLeftPos = GameObject.Find("UpperLeftCheck").transform;
+        upperRightPos = GameObject.Find("UpperRightCheck").transform;
+        lowerLeftPos = GameObject.Find("BottomLeftCheck").transform;
+        lowerRightPos = GameObject.Find("BottomRightCheck").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance1 = upperLeft.position - upperLeftPos.position;
+        float magnitude = distance1.magnitude;
+        if (magnitude < .25) {
+            paperUpperLeft = true;
+            upperLeft.position = upperLeftPos.position;
+        }
+
+        Vector3 distance2 = upperRight.position - upperRightPos.position;
+        magnitude = distance2.magnitude;
+        if (magnitude < .25)
+        {
+            paperUpperRight = true;
+            upperRight.position = upperRightPos.position;
+        }
+
+        Vector3 distance3 = lowerLeft.position - lowerLeftPos.position;
+        magnitude = distance3.magnitude;
+        if (magnitude < .25)
+        {
+            paperLowerLeft = true;
+            lowerLeft.position = lowerLeftPos.position;
+        }
+
+        Vector3 distance4 = lowerRight.position - lowerRightPos.position;
+        magnitude = distance4.magnitude;
+        if (magnitude < .25)
+        {
+            paperLowerRight = true;
+            lowerRight.position = lowerRightPos.position;
+        }
+
+        if (paperLowerLeft && paperLowerRight && paperUpperLeft && paperUpperRight) {
+            if (!text) {
+                text = dMan.ShowBox(puzzleComplete);
+                text = false;
+            
+            }
+        }
+    }
+}
